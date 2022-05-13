85967b353667757c850f5b1a0a396db04eaf12b4
Maximiliano Fiandrino <maxi.fiandrino@gmail.com>
2022-05-13 13:50:14 -0300

    Saco las validaciones de Color porque asumo que se va a elegir el color desde una interfaz visual que genere automaticamente las cantidades de RGB

--- 02-QueMePongo1/src/main/java/quemepongo/Color.java
+++ 02-QueMePongo1/src/main/java/quemepongo/Color.java
@@ -6,9 +6,6 @@ public class Color {
   private Integer blue;
 
   public Color(Integer red, Integer green, Integer blue) {
-    if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255) {
-      throw new IllegalArgumentException("El valor de cada color debe ser entre 0 y 255");
-    }
     this.red = red;
     this.green = green;
     this.blue = blue;
