c48c5896fa0f40e491572c64b5191a121d762147
Maximiliano Fiandrino <maxi.fiandrino@gmail.com>
2022-05-13 13:47:07 -0300

    Modelo TipoDePrenda como un enum con atributo y comportamiento

--- 02-QueMePongo1/src/main/java/quemepongo/TipoDePrenda.java
+++ 02-QueMePongo1/src/main/java/quemepongo/TipoDePrenda.java
@@ -1,20 +1,20 @@
 package quemepongo;
 
-public class TipoDePrenda {
+public enum TipoDePrenda {
+
+  ZAPATO(Categoria.CALZADO),
+  ZAPATILLA(Categoria.CALZADO),
+  PANTALON(Categoria.INFERIOR),
+  SHORT(Categoria.INFERIOR),
+  REMERA(Categoria.SUPERIOR),
+  CAMISA(Categoria.SUPERIOR),
+  COLLAR(Categoria.ACCESORIO),
+  GOORA(Categoria.ACCESORIO);
   private Categoria categoria;
 
-  public TipoDePrenda(Categoria categoria) {
+  TipoDePrenda(Categoria categoria) {
     this.categoria = categoria;
   }
-
-  static TipoDePrenda ZAPATO = new TipoDePrenda(Categoria.CALZADO);
-  static TipoDePrenda ZAPATILLA = new TipoDePrenda(Categoria.CALZADO);
-  static TipoDePrenda PANTALON = new TipoDePrenda(Categoria.INFERIOR);
-  static TipoDePrenda SHORT = new TipoDePrenda(Categoria.INFERIOR);
-  static TipoDePrenda REMERA = new TipoDePrenda(Categoria.SUPERIOR);
-  static TipoDePrenda CAMISA = new TipoDePrenda(Categoria.SUPERIOR);
-  static TipoDePrenda COLLAR = new TipoDePrenda(Categoria.ACCESORIO);
-  static TipoDePrenda GORRA = new TipoDePrenda(Categoria.ACCESORIO);
 
   public Categoria getCategoria() {
     return categoria;
